3ccce380688501060a69f7d80d1cffdbe438a5bf
kata1 <black.smetana777@mail.ru>
2024-01-04 19:12:26 +0300

    PP_2_1_2_Spring 1. POJO-Class Cat. 2.AppConfig @bean ("cat") @Scope("prototype") 3.beanHelloWorld1 == beanHelloWorld2 (true)   beanCat3 == beanCat4 (false)

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld beanHellWorld1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanHellWorld2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanHellWorld1 == beanHellWorld2);
+        Cat beanCat3 = (Cat) applicationContext.getBean("cat");
+        Cat beanCat4 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat3== beanCat4);
     }
 }
